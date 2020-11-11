46291a862d4c33a02f704c152fe970e8996b97af
Rabiul Alam <rabiul.alam@outlook.com>
2020-11-11 14:51:55 -0500

    netlify

--- backend/app.js
+++ backend/app.js
@@ -1,75 +1,73 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const express = require('express');
-const mongoose = require('mongoose');
-const logger = require('morgan');
-const path = require('path');
-const cors = require('cors');
-const session = require('express-session');
-const passport = require('./config/passport');
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const path = require("path");
+const cors = require("cors");
+const session = require("express-session");
+const passport = require("./config/passport");
 
-
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/deploymentExample'
-console.log('Connecting DB to ', MONGODB_URI)
+const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/deploymentExample";
+console.log("Connecting DB to ", MONGODB_URI);
 
 mongoose
   .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then((x) => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
-  .catch((err) => console.error('Error connecting to mongo', err));
+  .then((x) =>
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  )
+  .catch((err) => console.error("Error connecting to mongo", err));
 
-const app_name = require('./package.json').name;
-const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+const app_name = require("./package.json").name;
+const debug = require("debug")(
+  `${app_name}:${path.basename(__filename).split(".")[0]}`
+);
 
 const app = express();
 
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:3000", "https://clientnetlify.netlify.app"] //Swap this with the client url 
+    origin: ["http://localhost:3000", "https://kata-royale.netlify.app"], //Swap this with the client url
   })
 );
-
-
-
 
 app.use(
   session({
     resave: false,
     saveUninitialized: true,
     secret: "secret",
-    cookie: { maxAge: 1000 * 60 * 60 }
+    cookie: { maxAge: 1000 * 60 * 60 },
   })
 );
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-
-app.use(express.static(path.join(__dirname, '../frontend/build')))
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(logger('dev'));
+app.use(logger("dev"));
 
-const index = require('./routes/index');
-const auth = require('./routes/auth');
+const index = require("./routes/index");
+const auth = require("./routes/auth");
 
+app.use("/api", index);
+app.use("/api", auth);
 
-app.use('/api', index);
-app.use('/api', auth);
+app.get("*", (req, res, next) => {
+  console.log("weird", req.headers.host, "peach", req.url);
 
-
-app.get('*', (req, res, next) => {
-  console.log('weird', req.headers.host, 'peach', req.url)
-
-  if(req.headers.host.includes('heroku')){ 
-    res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+  if (req.headers.host.includes("heroku")) {
+    res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
   } else {
-    next()
+    next();
   }
-})
+});
 
 module.exports = app;
